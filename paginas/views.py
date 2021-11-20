2d61ba14144e7a39ca23df62a387f0cce54e99b6
Varjus <75344148+Varjus@users.noreply.github.com>
2021-11-20 20:55:05 -0300

    render views

--- paginas/views.py
+++ paginas/views.py
@@ -1,9 +1,18 @@
 # from django.shortcuts import render, HttpResponse
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 
 class PaginaInicial(TemplateView):
     template_name = "index.html"
 
+    def index(request):
+        return render(request, "index.html")
+
+
 class HomeView(TemplateView):
     template_name = "home.html"
+
+
+    def index(request):
+        return render(request, "home.html")
