969b1653571ea8f73e2ab313c4fcd9805675178f
Varjus <75344148+Varjus@users.noreply.github.com>
2021-11-20 11:04:04 -0300

    definições de pastas

--- descartezap/settings/base.py
+++ descartezap/settings/base.py
@@ -135,6 +135,7 @@ STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = (BASE_DIR / "staticfiles")
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
