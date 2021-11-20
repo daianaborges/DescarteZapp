5464208b17fce808ac1089963d7b6ee2c59e49c3
Varjus <75344148+Varjus@users.noreply.github.com>
2021-11-20 00:36:51 -0300

    alterado definicoes em settings

--- descartezap/settings/base.py
+++ descartezap/settings/base.py
@@ -16,7 +16,7 @@ import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -134,8 +134,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATIC_ROOT = (BASE_DIR / "staticfiles")
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 # django-allauth
